28a680b5a6e37f87d508383c17a9bf058483659d
jordin <jordinp062@gmail.com>
2025-03-30 18:30:27 +0200

    navbar hecha

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -1,10 +1,10 @@
 <header>
     <nav>
         <ul>
-            <li><a href="../formPost.php">Post</a></li>
-            <li><a href="../selectTable.php">Select</a></li>
-            <li><a href="../updateDatos.php">Update</a></li>
-            <li><a href="../deleteTable.php">Delete</a></li>
+            <li><a href="../forms/formPost.php">Post</a></li>
+            <li><a href="../forms/selectTable.php">Select</a></li>
+            <li><a href="../forms/updateDatos.php">Update</a></li>
+            <li><a href="../forms/deleteTable.php">Delete</a></li>
         </ul>
     </nav>
 </header>
