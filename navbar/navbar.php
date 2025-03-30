a54b6ee2a07c0f69b1a0079395813ea9b1418c7c
jordin <jordinp062@gmail.com>
2025-03-30 18:30:02 +0200

    navbar hecha

---
+++ navbar/navbar.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="../formPost.php">Post</a></li>
+            <li><a href="../selectTable.php">Select</a></li>
+            <li><a href="../updateDatos.php">Update</a></li>
+            <li><a href="../deleteTable.php">Delete</a></li>
+        </ul>
+    </nav>
+</header>
