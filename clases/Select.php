c75861f0e7c8a3986ec8fe890b44aff70d2cc941
jordin <jordinp062@gmail.com>
2025-03-30 16:16:02 +0200

    Metodo select terminado

---
+++ clases/Select.php
@@ -0,0 +1,32 @@
+<?php 
+
+/**
+ * @method table => me devuelve una tabla
+ */
+class Select 
+{
+    private $conexion;
+
+    public function __construct($conexion){
+        $this->conexion = $conexion;
+    }
+
+    public function get()
+    {
+        try{
+            $sql = "SELECT * FROM posts";
+            $resultado = $this->conexion->query($sql);
+            $filas = $resultado->fetchAll();
+
+            if(!empty($filas)){
+                return $filas;
+            }else{
+                throw new Exception("No hay datos en la base de datos");
+            }
+        }catch(PDOException $e){
+            return $e->GetMessage();
+        }finally{
+            $this->conexion=null;
+        }
+    }
+}
---
+++ forms/selectTable.php
@@ -0,0 +1,39 @@
+<?php 
+    require_once "../conexionGbd/conexion.php";
+    require_once "../clases/Select.php";
+
+    $get = new Select($conexion);
+    $select = $get->get();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select</title>
+</head>
+<body>
+    <h1 style="color:#48e">All the news</h1>
+
+    <table border="1">
+        <tr>
+            <th>Id new</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Publication date</th>
+            <th>Publiaction</th>
+        </tr>
+
+        <?php foreach ($select as $value) {?>
+            <tr>
+                <td><?= $value["id"] ?></td>
+                <td><?= $value["title"] ?></td>
+                <td><?= $value["description"] ?></td>
+                <td><?= $value["publication_date"] ?></td>
+                <td><?= $value["publication"] ?></td>
+            </tr>
+        <?php }?>
+    </table>
+</body>
+</html>
